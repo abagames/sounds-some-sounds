41a1d9f6d34618ec2a62c48bedbce9255e9e065a
ABA Games <cs8k-cyu@asahi-net.or.jp>
2016-05-28 17:13:14 +0900

    fix to declare as a module

--- typings/sss/sss.d.ts
+++ typings/sss/sss.d.ts
@@ -1,6 +1,7 @@
-declare module sss {
+declare module 'sss' {
   function init(_seed?: number, tempo?: number, fps?: number);
-  function play(name?: string, mult?: number, params? :any);
+  function setSeed(_seed?: number);
+  function play(name?: string, mult?: number, params?: any);
   function setVolume(volume: number);
   function setQuantize(_quantize: number);
   function playBgm(name?: string, interval?: number, params?: any);
