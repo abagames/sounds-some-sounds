690e34195182d7d1f34f5b1d4b227c31ec664b22
ABA Games <cs8k-cyu@asahi-net.or.jp>
2018-06-17 16:30:22 +0900

    Update typings

--- web_typings/sounds-some-sounds/index.d.ts
+++ web_typings/sounds-some-sounds/index.d.ts
@@ -4,16 +4,30 @@ declare module "sounds-some-sounds" {
   function play(
     name?: string,
     numberOfSounds?: number,
+    note?: number,
     params?: any,
     volume?: number
   );
+  function playJingle(
+    name?: string,
+    isSe?: boolean,
+    note?: number,
+    len?: number,
+    interval?: number,
+    numberOfTracks?: number,
+    param?: any,
+    volume?: number
+  );
+  function stopJingles();
   function setVolume(volume: number);
   function setQuantize(_quantize: number);
   function playBgm(
     name?: string,
+    note?: number,
+    len?: number,
     interval?: number,
+    numberOfTracks?: number,
     params?: any,
-    tracksNum?: number,
     volume?: number
   );
   function stopBgm();
